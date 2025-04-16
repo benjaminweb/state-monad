f286f0c8973d135b19f1a614f6f71d38b4d3f1d1
Benjamin Weber <one@bwe.im>
2025-04-16 15:48:34 +0200

    complete (>>=) for `State s` and correct type signatures

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 module MyLib (someFunc) where
 
 someFunc :: IO ()
@@ -12,23 +13,25 @@ someFunc = putStrLn "someFunc"
 newtype State s a = MkState (s -> (a, s))
 
 instance Functor (State s) where
--- fmap :: (a -> b) -> f a                 -> f b
--- fmap :: (a -> b) -> State s a           -> State s b
--- fmap :: (a -> b) -> State (s -> (a, s)) -> State (s -> (b, s))
-   fmap    f           (MkState g)         =  MkState $ \s0 -> let (a, s1) = g s0 in (f a, s1)
+-- fmap :: (a -> b) -> f a                     -> f b
+   fmap :: (a -> b) -> State s a               -> State s b
+-- fmap :: (a -> b) -> MkState (s0 -> (a, s1)) -> MkState (s0 -> (l, s1))
+   fmap    f           (MkState g)             =  MkState $ \s0 -> let (a, s1) = g s0 in (f a, s1)
 
 instance Applicative (State s) where
--- <*> :: f (a -> b)                   -> f a                     -> f b
--- <*> :: State s (a -> b)             -> State s a               -> State s b
--- <*> :: MkState (s0 -> (a -> b, s1)) -> MkState (s0 -> (a, s1)) -> MkState (s0 -> (b, s1))
-   (<*>)  (MkState f)                     (MkState g)             =  MkState $ \s0 -> let (f', s1)   = f s0
-                                                                                          (a, s2)    = g s1
-                                                                                       in (f' a, s2)
+-- (<*>) :: f (a -> b)                   -> f a                     -> f b
+   (<*>) :: State s (a -> b)             -> State s a               -> State s b
+-- (<*>) :: MkState (s0 -> (a -> b, s1)) -> MkState (s0 -> (a, s1)) -> MkState (s0 -> (b, s1))
+   (<*>)  (MkState f)                     (MkState g)                =  MkState $ \s0 -> let (f', s1)   = f s0
+                                                                                             (a, s2)    = g s1
+                                                                                         in (f' a, s2)
 instance Monad (State s) where
 -- (>>=) :: Monad m => m a                   -> (a -> m b)                   -> m b
--- (>>=) :: Monad m => State s a             -> (a -> State s b)             -> State s b
--- (>>=) :: Monad m => MkState (s -> (a, s)) -> (a -> MkState (s -> (b, s))) -> MkState (s -> (b, s))
-(>>=)                  (MkState f)              g                             = MkState $ \s0 -> let (a, s1) = f s0 in g a
+   (>>=) ::                                     State s a                    -> (a -> State s b)             -> State s b
+-- (>>=) ::                                     MkState (s -> (a, s))        -> (a -> MkState (s -> (b, s))) -> MkState (s -> (b, s))
+   (>>=)                                        (MkState f)                     g                             = MkState $ \s0 -> let (a, s1)    = f s0
+                                                                                                                                     MkState g' = g a
+                                                                                                                                  in g' s1
 
 
 return :: a    -> State s a

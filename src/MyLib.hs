90297a404f524e4e1e2d27b8fc94a69863ff6914
Benjamin Weber <one@bwe.im>
2025-04-16 15:08:07 +0200

    add Functor for `State s a`

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -3,22 +3,28 @@ module MyLib (someFunc) where
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+-- task 1:
 -- [exa] bwe: tbh I'd recommend just biting the bullet and trying to reimplement the thing yourself, it's most effective
 -- [exa] bwe: as the initial hint you get the information that stateful functions can be represented as (s -> (result, s)), possibly with
 --   more parameters such as (a->b->s->(result, s)), then you add Functor Applicative Monad instances and eventually it just works
 
-type State s a = (s -> (a, s))
+-- type State s a = (s -> (a, s))
+newtype State s a = State (s -> (a, s))
 
-(>>=) :: State s a -> (a -> State s b)      -> State s b
---       f         ->  g                    -> _
---       f         >>= g                    -> (s -> (b, s))
---   (s -> (a, s)) >>= (a -> (s -> (b, s))) -> (s -> (b, s))
--- f               >>= g                    = \s -> g _a _s
--- f               >>= g                    = \s -> let (a, s') = f s in g _a _s
--- f               >>= g                    = \s -> let (a, s') = f s in g a s'
-f                  >>= g                    = \s -> let (x, s') = f s in g x s'
+instance Functor (State s) where
+-- fmap :: (a -> b) -> f a                 -> f b
+-- fmap :: (a -> b) -> State s a           -> State s b
+-- fmap :: (a -> b) -> State (s -> (a, s)) -> State (s -> (b, s))
+   fmap    f           (State g)           =  State $ \s0 -> let (a, s1) = g s0 in (f a, s1)
+
+--instance Monad State s where
+--(>>=) :: State s a       -> (a -> State s b)              -> State s b
+--       f               ->  g                            -> _
+--       f               >>= g                            -> (s -> (b, s))
+--   State (s -> (a, s)) >>= (a -> (State (s -> (b, s)))) -> State (s -> (b, s))
+--  f                   >>= g                            = State (\s -> let (a, s') = f s in g a)
 
 return :: a    -> State s a
 -- return :: a -> (s -> (a, s))
 -- return x = \s -> _ :: (a, s)
-return x    = \s -> (x, s)
+return x    = State $ \s -> (x, s)

2c4e504fb333b07018513d5320d8f66f88de237b
Benjamin Weber <one@bwe.im>
2025-04-16 14:20:29 +0200

    add bind for `State s a`

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -7,4 +7,13 @@ someFunc = putStrLn "someFunc"
 -- [exa] bwe: as the initial hint you get the information that stateful functions can be represented as (s -> (result, s)), possibly with
 --   more parameters such as (a->b->s->(result, s)), then you add Functor Applicative Monad instances and eventually it just works
 
-type State s r = (s -> (r, s))
+type State s a = (s -> (a, s))
+
+(>>=) :: State s a -> (a -> State s b)      -> State s b
+--       f         ->  g                    -> _
+--       f         >>= g                    -> (s -> (b, s))
+--   (s -> (a, s)) >>= (a -> (s -> (b, s))) -> (s -> (b, s))
+-- f               >>= g                    = \s -> g _a _s
+-- f               >>= g                    = \s -> let (a, s') = f s in g _a _s
+-- f               >>= g                    = \s -> let (a, s') = f s in g a s'
+f                  >>= g                    = \s -> let (x, s') = f s in g x s'

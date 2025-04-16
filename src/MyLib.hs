d542c67bc7dd5b3962eacc4aaa11d6d65e0b80d5
Benjamin Weber <one@bwe.im>
2025-04-16 18:07:24 +0200

    fix type (comment) annotations

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -15,16 +15,16 @@ newtype State s a = MkState {runState :: s -> (a, s)}
 instance Functor (State s) where
 -- fmap :: (a -> b) -> f a                     -> f b
    fmap :: (a -> b) -> State s a               -> State s b
--- fmap :: (a -> b) -> (s0 -> (a, s1))         -> (s0 -> (b, s1))
+-- fmap :: (a -> b) -> (s -> (a, s))           -> (s -> (b, s))
    fmap    f           (MkState g)             =  MkState $ \s0 -> let (a, s1) = g s0 in (f a, s1)
 
 instance Applicative (State s) where
 -- (<*>) :: f (a -> b)                   ->  f a                     -> f b
    (<*>) :: State s (a -> b)             ->  State s a               -> State s b
--- (<*>) :: (s0 -> (a -> b, s1))         ->  (s1 -> (a, s2))         -> (s2 -> (b, s3))
-   (MkState f)                           <*> (MkState g)             =  MkState $ \s0 -> let (f', s1)   = f s0
-                                                                                             (a, s2)    = g s1
-                                                                                         in (f' a, s2)
+-- (<*>) :: (s -> (a -> b, s))           ->  (s -> (a, s))           -> (s -> (b, s))
+   MkState f                             <*> MkState g             =  MkState $ \s0 -> let (f', s1)   = f s0
+                                                                                           (a, s2)    = g s1
+                                                                                       in  (f' a, s2)
 -- pure :: a -> f a
    pure :: a -> State s a
    pure    a =  MkState $ \s -> (a, s)
@@ -35,7 +35,7 @@ instance Monad (State s) where
 -- (>>=) :: (s -> (a, s))         ->  (a -> (s -> (b, s)))         -> (s -> (b, s))
    (MkState f)                    >>= g                            =  MkState $ \s0 -> let (a, s1)    = f s0
                                                                                            MkState g' = g a
-                                                                                       in g' s1
+                                                                                       in  g' s1
 
 -- task 2:
 -- [exa]: bwe: as the next task try to think how to implement "set state" function and "get state" function
